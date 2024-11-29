82d2cb39ffdd98de2bca263aa75c8458d4f77ac2
nothingnowhere1 <danirchis@mail.ru>
2024-11-30 01:52:04 +0300

    test build

--- bro.config.js
+++ bro.config.js
@@ -1,22 +1,22 @@
-const pkg = require("./package");
+const pkg = require('./package');
 
 module.exports = {
-  apiPath: "stubs/api",
-  webpackConfig: {
-    output: {
-      publicPath: `/static/${pkg.name}/${process.env.VERSION || pkg.version}/`,
+    apiPath: 'stubs/api',
+    webpackConfig: {
+        output: {
+            publicPath: `/static/${pkg.name}/${process.env.VERSION || pkg.version}/`
+        }
     },
-  },
-  /* use https://admin.bro-js.ru/ to create config, navigations and features */
-  navigations: {
-    "sberhub.main": "/sberhub",
-  },
-  features: {
-    "sberhub": {
-      // add your features here in the format [featureName]: { value: string }
+    /* use https://admin.bro-js.ru/ to create config, navigations and features */
+    navigations: {
+        'ConnectMe.main': '/ConnectMe'
     },
-  },
-  config: {
-    "sberhub.api": "/api",
-  },
+    features: {
+        'ConnectMe': {
+            // add your features here in the format [featureName]: { value: string }
+        }
+    },
+    config: {
+        'ConnectMe.api': '/api'
+    }
 };
