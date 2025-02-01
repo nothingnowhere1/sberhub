c45f2fdc5b81c2e5b884fd2a0bb61b0545d4e21c
nothingnowhere1 <danirchis@mail.ru>
2025-02-01 14:26:03 +0300

    1. change url

--- src/common/components/Inputs/CityInput.tsx
+++ src/common/components/Inputs/CityInput.tsx
@@ -4,7 +4,6 @@ import {MenuItem} from '@mui/material';
 import {SelectProps as BaseProps} from '@mui/material/Select/Select';
 import {SelectInput} from "./SelectInput";
 import axios from "axios";
-import {API_CITY} from "../../../Env";
 
 export type ControlledFieldProps<T extends FieldValues> = {
     control: Control<T>;
@@ -16,7 +15,7 @@ export const CityInput = <T extends FieldValues>(props: ControlledFieldProps<T>)
     const [dataCities, setDataCities] = useState<{ id: number; title: string }[] | null>(null);
 
     useEffect(() => {
-        axios.get(`${API_CITY}/cities`)
+        axios.get(`https://dev.bro-js.ru/connectme/api/cities`)
             .then(r => r.data)
             .then(r => setDataCities(r));
     }, []);
